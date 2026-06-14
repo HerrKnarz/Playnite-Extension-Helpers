fca5e3d93bf2430b76d9749da83b6239c1c584f5
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2026-06-14 22:06:21 +0200

    feat(settings): added settings controls to unify the look and feel of all my addon settings.

---
+++ src/UICommon/Controls/SettingsSection.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace PlayniteExtensionHelpers.UICommon.Controls;
+
+public class SettingsSection : SettingsBaseControl
+{
+    public static readonly DependencyProperty HelpCommandProperty =
+        DependencyProperty.Register(
+            "HelpCommand",
+            typeof(RelayCommand),
+            typeof(SettingsSection));
+
+    public RelayCommand HelpCommand
+    {
+        get => (RelayCommand)GetValue(HelpCommandProperty);
+        set => SetValue(HelpCommandProperty, value);
+    }
+}
