a9380cb39a0c2f69360dffdfc3bafd1cbffa3e26
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2026-06-15 21:01:31 +0200

    feat(ui): Added ScrollIntoView behavior

---
+++ src/UICommon/ListBoxScrollIntoViewBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+using ListBox = System.Windows.Controls.ListBox;
+
+namespace PlayniteExtensionHelpers.UICommon;
+
+/// <summary>
+/// A behavior that automatically scrolls the selected item of a ListBox into view when the
+/// selection changes.
+/// </summary>
+public class ScrollIntoViewBehavior : Behavior<ListBox>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.SelectionChanged += OnSelectionChanged;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.SelectionChanged -= OnSelectionChanged;
+    }
+
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (AssociatedObject.SelectedItem != null)
+        {
+            var listBox = sender as ListBox;
+
+            if (listBox?.SelectedItem != null)
+            {
+                listBox.Dispatcher.BeginInvoke(
+                    () =>
+                    {
+                        listBox.UpdateLayout();
+
+                        if (listBox.SelectedItem != null)
+                        {
+                            listBox.ScrollIntoView(listBox.SelectedItem);
+                        }
+                    });
+            }
+        }
+    }
+}
